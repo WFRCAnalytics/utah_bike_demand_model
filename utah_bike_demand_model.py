28dc3c8f9585ca16ca1f6bb34819aa9de9979aba
Blake Rosenthal <blake.rosenthal@rsginc.com>
2021-03-08 16:38:49 -0800

    compatibility with micromobility v0.2.1

--- utah_bike_demand_model.py
+++ utah_bike_demand_model.py
@@ -1,35 +1,27 @@
 """Run the Utah bike model
 
-This script runs the models in the micromobility_toolset on
-a 25-zone example data set.
+This script runs the Utah Bike Demand models on a given set of inputs.
 
 It instantiates a driver class called 'Scenario' that keeps track of the 'config',
-'data', 'output' directories for the base and build scenarios. These directories
+'input', 'output' directories for model run. These directories
 contain all the necessary instructions and data to run the model.
 
   - 'configs' contains settings files for each of the core elements of the
   model. These are .yaml files that provide constants and configuration
   options for the network, skims, landuse data, and trip data.
 
-  - 'data' contains the initial data before any models are run. It
-  contains initial trip tables and land use (TAZ) data in either
-  CSV or SQLITE format.
-  
-The `model.filter_impact_area(...)` utility will either precompute the differences in
-network and zone data between two scenarios or filter the zones given a list of
-zone IDs. Subsequent calculations will only
-generate skims and trips for zones affected by the differences. Commenting out
-this line will compute OD pairs for every zone in the input files.
+  - 'inputs' contains the initial data before any models are run. It
+  contains land use data and link/node network data.
 
-The models will generally attempt to load trip/skim data found in the 'data'
+The models will generally attempt to load trip/skim data found in the 'input'
 directory to perform their calculations. If the required data
 cannot be found, the model will create the necessary files with the data it
-is provided.
+is provided and save the results to the 'output' directory. These files can be moved
+to the 'input' folder and reused for subsequent runs.
 """
 
 
 import argparse
-import numpy as np
 
 from micromobility_toolset import model
 from micromobility_toolset.network import preprocessor
@@ -38,39 +30,42 @@ from micromobility_toolset.network import preprocessor
 def main():
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', dest='name', action='store', choices=model.list_steps())
-    parser.add_argument('--sample', dest='sample', action='store', type=int)
+    parser.add_argument(
+        "--step", "--name", dest="step", action="store", choices=model.list_steps()
+    )
+    parser.add_argument("--sample", dest="sample", action="store", type=int)
     args = parser.parse_args()
 
-    utah_base = model.Scenario(
-        name='Utah Base Scenario',
-        config='Model_Configs',
-        inputs='Model_Inputs',
-        outputs='Model_Outputs')
+    model.config_logger()
+    utah_scenario = model.Scenario(
+        name="Utah Scenario",
+        config="Model_Configs",
+        inputs="Model_Inputs",
+        outputs="Model_Outputs",
+    )
 
-    # only use first 100 microzones for testing. remove to run full dataset.
-    # this method can also be used to compare two scenarios.
-    if args.sample:
-        model.filter_impact_area(utah_base, zone_ids=list(range(0, args.sample)))
-
-    if args.name:
-        model.run(args.name, utah_base)
+    if args.step:
+        model.run(args.step, utah_scenario)
 
     else:
-        model.run('generate_demand', utah_base)
-        model.run('assign_demand', utah_base)
+        model.run("skim_network", utah_scenario)
+        model.run("generate_demand", utah_scenario)
+        model.run("assign_demand", utah_scenario)
 
 
 @preprocessor()
 def preprocess_network(net):
-    """add network attributes that are combinations of existing attributes"""
-    
-    distance = np.array(net.graph.es['distance'], dtype='float')
-    slope = np.array(net.graph.es['distance'], dtype='float')
-    bike_blvd = np.array(net.graph.es['bike_boulevard'], dtype='bool')
-    bike_path = np.array(net.graph.es['bike_path'], dtype='bool')
-    bike_lane = np.array(net.graph.es['bike_lane'], dtype='bool')
-    aadt = np.array(net.graph.es['AADT'], dtype='float')
+    """
+    Add 'bike_cost' network attribute as a combination of existing attributes,
+    including turns.
+    """
+
+    distance = net.get_edge_values("distance", dtype="float")
+    slope = net.get_edge_values("distance", dtype="float")
+    bike_blvd = net.get_edge_values("bike_boulevard", dtype="bool")
+    bike_path = net.get_edge_values("bike_path", dtype="bool")
+    bike_lane = net.get_edge_values("bike_lane", dtype="bool")
+    aadt = net.get_edge_values("AADT", dtype="float")
 
     light = (10e3 < aadt) & (aadt < 20e3)
     med = (20e3 <= aadt) & (aadt < 30e3)
@@ -80,17 +75,17 @@ def preprocess_network(net):
     med_slope = (4.0 <= slope) & (slope < 6.0)
     big_slope = 6.0 < slope
 
-    turn = np.array(net.graph.es['turn'], dtype='bool')
-    signal = np.array(net.graph.es['traffic_signal'], dtype='bool')
-    turn_type = np.array(net.graph.es['turn_type'])
-    parallel_aadt = np.array(net.graph.es['parallel_aadt'], dtype='float')
-    cross_aadt = np.array(net.graph.es['cross_aadt'], dtype='float')
+    turn = net.get_edge_values("turn", dtype="bool")
+    signal = net.get_edge_values("traffic_signal", dtype="bool")
+    turn_type = net.get_edge_values("turn_type", dtype="str")
+    parallel_aadt = net.get_edge_values("parallel_aadt", dtype="float")
+    cross_aadt = net.get_edge_values("cross_aadt", dtype="float")
 
-    left = turn_type == 'left'
-    left_or_straight = (turn_type == 'left') | (turn_type == 'straight')
-    right = turn_type == 'right'
+    left = turn_type == "left"
+    left_or_straight = (turn_type == "left") | (turn_type == "straight")
+    right = turn_type == "right"
 
-    light_cross= (5e3 < cross_aadt) & (cross_aadt < 10e3)
+    light_cross = (5e3 < cross_aadt) & (cross_aadt < 10e3)
     med_cross = (10e3 <= cross_aadt) & (cross_aadt < 20e3)
     heavy_cross = 20e3 <= cross_aadt
 
@@ -98,32 +93,34 @@ def preprocess_network(net):
     heavy_parallel = 20e3 <= parallel_aadt
 
     # distance coefficients
-    bike_cost = \
-        distance * (
-            1.0 + \
-            (bike_blvd * -0.108) + \
-            (bike_path * -0.16) + \
-            (small_slope * 0.371) + \
-            (med_slope * 1.23) + \
-            (big_slope * 3.239) + \
-            (bike_lane * med * 0.25) + \
-            (bike_lane * heavy * 1.65) + \
-            (~bike_lane * light * 0.368) + \
-            (~bike_lane * med * 1.4) + \
-            (~bike_lane * heavy * 7.157))
+    bike_cost = distance * (
+        1.0
+        + (bike_blvd * -0.108)
+        + (bike_path * -0.16)
+        + (small_slope * 0.371)
+        + (med_slope * 1.23)
+        + (big_slope * 3.239)
+        + (bike_lane * med * 0.25)
+        + (bike_lane * heavy * 1.65)
+        + (~bike_lane * light * 0.368)
+        + (~bike_lane * med * 1.4)
+        + (~bike_lane * heavy * 7.157)
+    )
 
     # fixed-cost penalties
-    bike_cost += \
-        (turn * 0.034) + \
-        (signal * 0.017) + \
-        (left_or_straight * light_cross * 0.048) + \
-        (left_or_straight * med_cross * 0.05) + \
-        (left_or_straight * heavy_cross * 0.26) + \
-        (right * heavy_cross * 0.031) + \
-        (left * med_parallel * 0.073) + \
-        (left * heavy_parallel * 0.18)
+    bike_cost += (
+        (turn * 0.034)
+        + (signal * 0.017)
+        + (left_or_straight * light_cross * 0.048)
+        + (left_or_straight * med_cross * 0.05)
+        + (left_or_straight * heavy_cross * 0.26)
+        + (right * heavy_cross * 0.031)
+        + (left * med_parallel * 0.073)
+        + (left * heavy_parallel * 0.18)
+    )
 
-    net.graph.es['bike_cost'] = np.nan_to_num(bike_cost)
+    net.set_edge_values("bike_cost", bike_cost)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
